92910da665f98ea52c8f1f8b5b405830daecbca9
Chayel Heinsen <chayel@chayelheinsen.com>
2018-01-18 08:53:40 -0500

    Add password to cms

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -8,16 +8,14 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    CMS_PASSWORD = ENV.fetch("CMS_PASSWORD")
+
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      authenticate_or_request_with_http_basic do |_, password|
+        password == CMS_PASSWORD
+      end
     end
-
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
   end
 end
