3de50f4f5e8f3711ec2909e3ecf7e6bf5036e500
Chayel Heinsen <chayel@chayelheinsen.com>
2018-01-28 17:53:09 -0500

    Add opengraph meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     return "active" if request.path == test_path
     ""
   end
+
+  def meta_tag(tag, text)
+    content_for :"meta_#{tag}", text
+  end
+
+  def yield_meta_tag(tag, default_text='')
+    content_for?(:"meta_#{tag}") ? content_for(:"meta_#{tag}") : default_text
+  end
 end
